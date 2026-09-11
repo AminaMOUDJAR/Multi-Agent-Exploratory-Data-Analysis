1048432b20b2c19f64ba2fa539b9a9dba1d32a13
AMINA MOUDJAR <moudjaramina2001@gmail.com>
2026-09-11 16:38:47 +0100

    Delete start_frontend.bat

--- start_frontend.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem Start the React dev server on http://localhost:5173
-cd /d "%~dp0frontend"
-if not exist node_modules (
-  echo Installing npm dependencies...
-  call npm install
-)
-npm run dev
